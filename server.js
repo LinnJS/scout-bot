dd4278ace07ce4540a4911fdd00da29e9f4883e2
JSLinn727 <justinslinn727@gmail.com>
2017-08-17 16:57:04 -0400

    Defined DD as key and DT as values into their own objects

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const fs = require('fs')
 const app = express()
 const port = 3000
 
-
 // let destination = fs.createWriteStream('./downloads/google2.html')
 // let url = 'https://google.com'
 // request(url)
@@ -24,25 +23,36 @@ const port = 3000
 let url = 'http://archive.thedali.org/mwebcgi/mweb.exe?request=record;id=169;type=101'
 let destination = fs.createWriteStream('./downloads/dali.txt')
 
-
-request(url, function(err, res, body){
+request(url, function(err, res, body) {
   let $ = cheerio.load(body)
   let details = $('.details dl')
   let detailsText = details.text()
+  let key = ''
+  let value = ''
+  let obj = {}
   details.children().each((i, node) => {
-    console.log(node.name, $(node).text())
+    ;`$(dt) space $(dd)`
+    if (node.name === 'dt') {
+      value = $(node).text()
+    } else {
+      key = $(node).text()
+    }
+    if (key !== '' && value !== '') {
+      obj[key] = value
+      key = ''
+      value = ''
+    }
+
+    //console.log(node.name, $(node).text())
   })
-// details.children().for
-
-  // .pipe(destination)
-  // .on('finish', function(){
-  //   console.log('done')
-  // })
-  // .on('err', function(err){
-  //   console.log(err)
-  // })
-  console.log(detailsText)
+  console.log(obj)
 })
-
+// }).pipe(destination)
+// .on('finish', function(){
+//   console.log('done')
+// })
+// .on('err', function(err){
+//   console.log(err)
+// })
 app.listen(port)
 console.log(`server is listening on port ${port}`)
